112a3468ffecabebf57da76002b1ac2e91cbb5fd
fffussel <lucasschladitz@gmail.com>
2025-08-31 09:46:48 +0200

    add basic input options singleton

---
+++ Scripts/Globals/controlls.gd
@@ -0,0 +1,9 @@
+extends Node
+
+enum input_method {CONTROLLER, KEYBOARD}
+
+var p1_input_method: input_method
+var p1_device_id: String
+
+var p2_input_method: input_method
+var p2_device_id: String
